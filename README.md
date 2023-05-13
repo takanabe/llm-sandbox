cbefe79d0294cca850d173880fc24d449950db58
Takayuki Watanabe <takanabe.w@gmail.com>
2023-05-13 15:20:21 +0900

    implement word to english info using text completion

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # openai-sandbox
+
+## OpenAI's API reference
+
+Read https://platform.openai.com/docs/api-reference/authentication
+
+This page contains on how to use OpenAI's Python and Node libraries that make our development easy.
--- word-learner/server.py
+++ word-learner/server.py
@@ -18,6 +18,54 @@ def get_word_meaning(api_key, word):
     ret = chat_completion.choices[0].message.content
     return ret
 
+def get_word_information(api_key, word):
+
+    prompt = f"Please provide the following information for the English word'{word}':\n\n" \
+             f"1. English meaning\n" \
+             f"2. English example\n" \
+             f"3. Japanese meaning\n" \
+             f"4. Japanese example\n\n" \
+             f"Answer:\n" \
+             f"1. {{English meaning}}\n" \
+             f"2. {{English example}}\n" \
+             f"3. {{Japanese meaning}}\n" \
+             f"4. {{Japanese example}}"
+
+    data = {
+        "prompt": prompt,
+        "max_tokens": 200,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.5,
+    }
+
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=200,
+        temperature=0.5,
+    )
+
+    ret = completion.choices[0].text
+
+    # if answer:
+    #     word_info = parse_answer(answer)
+    #     table = PrettyTable()
+    #     table.field_names = ["ID", "Word", "English Meaning", "English Example", "Japanese Meaning", "Japanese Example"]
+    #     table.add_row([1, word, word_info["1"], word_info["2"], word_info["3"], word_info["4"]])
+    #     print(table)
+    # else:
+    #     print(f"Could not find the information for '{word}'")
+    return ret
+
+def parse_answer(answer):
+    lines = answer.split("\n")
+    parsed_info = {}
+    for line in lines:
+        key, value = line.split(".", 1)
+        parsed_info[key.strip()] = value.strip()
+    return parsed_info
+
 
 @app.post("/", response_class=PlainTextResponse)
 async def receive_message(request: Request):
@@ -34,7 +82,10 @@ async def receive_word(request: Request):
 
     data = await request.body()
     word = data.decode('utf-8')
-    meaning = get_word_meaning(api_key, word)
+
+    # Switch API
+    # meaning = get_word_meaning(api_key, word)
+    meaning = get_word_information(api_key, word)
 
     if meaning:
         return f"The meaning of '{word}' is: {meaning}"
